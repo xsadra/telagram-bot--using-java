1212971fd97b651763e5e2980ece8ee343f63031
Sadra <ti9mail@gmail.com>
2018-04-07 23:23:09 +0200

    add RestTemplate Class

---
+++ bot/src/main/java/de/sadrab/telagrambotusingjava/configuration/RestTemplateConfiguration.java
@@ -0,0 +1,13 @@
+package de.sadrab.telagrambotusingjava.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
